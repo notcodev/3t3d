01fedfa24653d034636e9243e51e5d5cb9b7f597
Erik <slmv_e@icloud.com>
2024-04-02 10:34:15 +0300

    🚀 perf: improved performance, now hover does not trigger rerender

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { OrbitControls, RoundedBox } from '@react-three/drei'
 import { Canvas, useFrame, Vector3 } from '@react-three/fiber'
 import { useUnit } from 'effector-react'
-import { useRef, useState } from 'react'
+import {
+  Dispatch,
+  PropsWithChildren,
+  SetStateAction,
+  createContext,
+  useContext,
+  useRef,
+  useState,
+} from 'react'
 import {
   BufferGeometry,
   MathUtils,
@@ -27,7 +35,7 @@ const Cell = ({
 }) => {
   const ref = useRef<Mesh<BufferGeometry<NormalBufferAttributes>> | null>(null)
 
-  const [hovered, setHovered] = useState(false)
+  const hovered = useRef(false)
   const { $shape: shape, cellClicked } = useUnit(
     model.field[selectCell(position)],
   )
@@ -36,7 +44,7 @@ const Cell = ({
     if (!ref.current) return
     ;(ref.current.material as MeshBasicMaterial).opacity = MathUtils.lerp(
       (ref.current.material as MeshBasicMaterial).opacity,
-      hovered ? 1 : 0.35,
+      hovered.current ? 1 : 0.35,
       0.1,
     )
   })
@@ -51,9 +59,11 @@ const Cell = ({
         }}
         onPointerOver={(event) => {
           event.stopPropagation()
-          setHovered(true)
+          hovered.current = true
         }}
-        onPointerOut={() => setHovered(false)}
+        onPointerOut={() => {
+          hovered.current = false
+        }}
         args={[BOX_SIZE, BOX_SIZE, BOX_SIZE]}
         radius={0.1}
         position={coordinates}
@@ -77,47 +87,89 @@ const Cell = ({
   )
 }
 
-export const App = () => {
-  const [opened, setOpened] = useState(false)
-  const gameState = useUnit(model.$gameState)
+const ExpandContext = createContext<{
+  expanded: boolean
+  setExpanded: Dispatch<SetStateAction<boolean>>
+} | null>(null)
 
-  const coordinates = getCoordinates(opened ? 1 : 0.25)
+const ExpantContextProvider = ({ children }: PropsWithChildren) => {
+  const [expanded, setExpanded] = useState(false)
 
   return (
-    <>
-      <button onClick={() => setOpened((prev) => !prev)}>
-        {opened ? 'Close' : 'Open'}
+    <ExpandContext.Provider value={{ expanded, setExpanded }}>
+      {children}
+    </ExpandContext.Provider>
+  )
+}
+
+const useExpandContext = () => {
+  const ctx = useContext(ExpandContext)
+
+  if (ctx === null) {
+    throw new Error(
+      'Hook "useExpandContext" called outside of ExpantContextProvider',
+    )
+  }
+
+  return ctx
+}
+
+const Overlay = () => {
+  const gameState = useUnit(model.$gameState)
+  const { expanded, setExpanded } = useExpandContext()
+
+  return (
+    <main id="overlay">
+      <button onClick={() => setExpanded((prev) => !prev)}>
+        {expanded ? 'Close' : 'Open'}
       </button>
       <span>{gameState}</span>
-      <Canvas camera={{ position: [5, 5, 5] }}>
-        <ambientLight intensity={Math.PI / 2} />
-        <directionalLight
-          castShadow
-          position={[10, 20, 15]}
-          shadow-camera-right={8}
-          shadow-camera-top={8}
-          shadow-camera-left={-8}
-          shadow-camera-bottom={-8}
-          shadow-mapSize-width={1024}
-          shadow-mapSize-height={1024}
-          intensity={2}
-          shadow-bias={-0.0001}
-        />
+    </main>
+  )
+}
 
-        {coordinates.map((x, indexX) =>
-          coordinates.map((y, indexY) =>
-            coordinates.map((z, indexZ) => (
-              <Cell
-                key={`${x}:${y}:${z}`}
-                coordinates={[x, y, z]}
-                position={[indexX, indexY, indexZ]}
-              />
-            )),
-          ),
-        )}
+const Scene = () => {
+  const { expanded } = useExpandContext()
+  const coordinates = getCoordinates(expanded ? 1 : 0.25)
 
-        <OrbitControls autoRotate={true} />
-      </Canvas>
-    </>
+  return (
+    <Canvas camera={{ position: [5, 5, 5] }}>
+      <ambientLight intensity={Math.PI / 2} />
+      <directionalLight
+        castShadow
+        position={[10, 20, 15]}
+        shadow-camera-right={8}
+        shadow-camera-top={8}
+        shadow-camera-left={-8}
+        shadow-camera-bottom={-8}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        intensity={2}
+        shadow-bias={-0.0001}
+      />
+
+      {coordinates.map((x, indexX) =>
+        coordinates.map((y, indexY) =>
+          coordinates.map((z, indexZ) => (
+            <Cell
+              key={`${x}:${y}:${z}`}
+              coordinates={[x, y, z]}
+              position={[indexX, indexY, indexZ]}
+            />
+          )),
+        ),
+      )}
+
+      <OrbitControls autoRotate={true} />
+    </Canvas>
+  )
+}
+
+export const App = () => {
+  return (
+    <ExpantContextProvider>
+      <Scene />
+      <Overlay />
+    </ExpantContextProvider>
   )
 }
--- src/constants/game.ts
+++ src/constants/game.ts
@@ -1,1 +1,1 @@
-export const possiblePositions = [0, 1, 2]
+export const possiblePositions = [0, 1, 2] as const
