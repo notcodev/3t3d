221b90e05c30187b6841f1b6051ab9eb1721868d
Erik <slmv_e@icloud.com>
2024-10-10 21:44:14 +0300

    🧹 chore: updated base eslint configuration file

--- base.eslintrc.cjs
+++ base.eslintrc.cjs
@@ -3,10 +3,5 @@ const { configure, presets } = require('eslint-kit')
 module.exports = configure({
   allowDebug: process.env.NODE_ENV !== 'production',
 
-  presets: [
-    presets.imports(),
-    presets.node(),
-    presets.prettier(),
-    presets.typescript(),
-  ],
+  presets: [presets.node(), presets.prettier(), presets.typescript()],
 })
