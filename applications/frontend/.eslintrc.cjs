fb3e5befff1ddeab178ec54131f98296e22399ff
Erik <slmv_e@icloud.com>
2024-10-10 13:27:39 +0300

    🔨 refactor: created monorepo and moved frontend to separated folder

--- applications/frontend/.eslintrc.cjs
+++ applications/frontend/.eslintrc.cjs
@@ -2,15 +2,8 @@ const { configure, presets } = require('eslint-kit')
 
 const eslintKitConfig = configure({
   allowDebug: process.env.NODE_ENV !== 'production',
-
-  presets: [
-    presets.imports(),
-    presets.node(),
-    presets.prettier(),
-    presets.typescript(),
-    presets.react(),
-    presets.effector(),
-  ],
+  extends: '../../base.eslintrc.cjs',
+  presets: [presets.react(), presets.effector()],
 })
 
 module.exports = {
