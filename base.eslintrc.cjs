d3954605512ab9a020039351164c38b600ee15e5
Erik <slmv_e@icloud.com>
2024-10-10 22:17:38 +0300

    🧹 chore: added ignore patterns to eslint config

--- base.eslintrc.cjs
+++ base.eslintrc.cjs
@@ -2,6 +2,9 @@ const { configure, presets } = require('eslint-kit')
 
 module.exports = configure({
   allowDebug: process.env.NODE_ENV !== 'production',
-
+  extend: {
+    root: true,
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+  },
   presets: [presets.node(), presets.prettier(), presets.typescript()],
 })
