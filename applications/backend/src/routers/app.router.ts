7ae6ab81af3d863b2f5688c60364676279f6313d
Erik <slmv_e@icloud.com>
2024-10-14 02:53:57 +0300

    ✨ feat: registerred router

--- applications/backend/src/routers/app.router.ts
+++ applications/backend/src/routers/app.router.ts
@@ -1,10 +1,12 @@
 import { t } from '../trpc'
 import { authRouter } from './auth.router'
+import { multiplayerGamesRouter } from './multiplayer-games.router'
 import { usersRouter } from './users.router'
 
 export const appRouter = t.router({
   users: usersRouter,
   auth: authRouter,
+  multiplayerGames: multiplayerGamesRouter,
 })
 
 export type AppRouter = typeof appRouter
