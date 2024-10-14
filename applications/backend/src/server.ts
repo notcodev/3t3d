6227f8d8375920b64ad89722373122474f3f15aa
Erik <slmv_e@icloud.com>
2024-10-14 19:53:54 +0300

    🔨 refactor: added port and host to listen method from config

--- applications/backend/src/server.ts
+++ applications/backend/src/server.ts
@@ -1,12 +1,16 @@
 import { buildFastify } from './app'
+import { globalConfig } from './configs/global.config'
 
 const app = await buildFastify()
 
-app.listen({ port: 3000 }, (error) => {
-  if (!error) {
-    app.log.info('Server successfully started')
-    return
-  }
+app.listen(
+  { port: globalConfig.fastify.port, host: globalConfig.fastify.host },
+  (error) => {
+    if (!error) {
+      app.log.info('Server successfully started')
+      return
+    }
 
-  process.exit(1)
-})
+    process.exit(1)
+  },
+)
