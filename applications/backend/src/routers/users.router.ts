395d4a75e6d26500d271b1f410404b58ddf284f7
Erik <slmv_e@icloud.com>
2024-10-15 01:07:24 +0300

    🔨 refactor: migrated get user logic to service

--- applications/backend/src/routers/users.router.ts
+++ applications/backend/src/routers/users.router.ts
@@ -1,22 +1,8 @@
-import { db } from '@drizzle/connection'
-import { users } from '@drizzle/schema'
-import { eq } from 'drizzle-orm'
-
+import * as usersService from '@/services/users.service'
 import { authorizedProcedure, t } from '@/trpc'
 
 export const usersRouter = t.router({
   getMe: authorizedProcedure.query(async ({ ctx: { session } }) => {
-    const selectedRows = await db
-      .select({
-        id: users.id,
-        username: users.username,
-        firstName: users.firstName,
-        lastName: users.lastName,
-        createdAt: users.createdAt,
-      })
-      .from(users)
-      .where(eq(users.id, session.userId))
-
-    return selectedRows[0]
+    return usersService.getUserById({ id: session.userId })
   }),
 })
---
+++ applications/backend/src/services/users.service.ts
@@ -0,0 +1,23 @@
+import { db } from '@drizzle/connection'
+import { users } from '@drizzle/schema'
+import { TRPCError } from '@trpc/server'
+import { eq } from 'drizzle-orm'
+
+export async function getUserById({ id }: { id: number }) {
+  const selectedRows = await db
+    .select({
+      id: users.id,
+      username: users.username,
+      firstName: users.firstName,
+      lastName: users.lastName,
+      createdAt: users.createdAt,
+    })
+    .from(users)
+    .where(eq(users.id, id))
+
+  if (selectedRows.length === 0) {
+    throw new TRPCError({ code: 'NOT_FOUND' })
+  }
+
+  return selectedRows[0]
+}
