2d8eada137a6843c7de01a2b260eab1b59fe16e4
Erik <slmv_e@icloud.com>
2024-10-14 23:24:30 +0300

    🐛 fix: added connect to database before server starts

--- applications/backend/src/server.ts
+++ applications/backend/src/server.ts
@@ -1,7 +1,11 @@
+import { connectToDatabase } from '@drizzle/connection'
+
 import { buildFastify } from './app'
 import { globalConfig } from './configs/global.config'
 
 const app = await buildFastify()
+
+await connectToDatabase()
 
 app.listen(
   { port: globalConfig.fastify.port, host: globalConfig.fastify.host },
