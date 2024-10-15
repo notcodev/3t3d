ddfc3192efcf1b1cb74fbe0879ca5a1a1699a8fb
Erik <slmv_e@icloud.com>
2024-10-15 19:32:23 +0300

    ✨ feat: added mock function utility

---
+++ applications/backend/src/utils/object-mock/mock-function.ts
@@ -0,0 +1,23 @@
+type AnyFunction = (...args: any[]) => any
+
+type OnlyFunctions<T extends object> = {
+  [Key in keyof T]: T[Key] extends AnyFunction ? T[Key] : never
+}
+
+export function mockFunction<Target extends object>(
+  target: Target,
+  field: keyof OnlyFunctions<Target>,
+  returns: ReturnType<OnlyFunctions<Target>[typeof field]>,
+) {
+  const targetInitial = target[field]
+  const mockFn = (..._args: unknown[]) => {
+    return returns
+  }
+  target[field] = mockFn as OnlyFunctions<Target>[typeof field]
+
+  return {
+    restore: () => {
+      target[field] = targetInitial
+    },
+  }
+}
