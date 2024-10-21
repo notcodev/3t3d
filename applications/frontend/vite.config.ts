7027d46afbc06939d3b0319a5ce792e4faa032a5
Erik <slmv_e@icloud.com>
2024-10-21 22:55:24 +0300

    ✨ feat: added router plugin

--- applications/frontend/vite.config.ts
+++ applications/frontend/vite.config.ts
@@ -1,10 +1,22 @@
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'node:path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    TanStackRouterVite({
+      routesDirectory: path.resolve(__dirname, 'src', 'app', 'routes'),
+      generatedRouteTree: path.resolve(
+        __dirname,
+        'src',
+        'app',
+        'routeTree.gen.ts',
+      ),
+    }),
+    react(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
