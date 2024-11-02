2c3855869d92605effe0ab7c473acc1030218668
Erik <slmv_e@icloud.com>
2024-11-02 03:47:14 +0300

    ✨ feat: created login and profile widgets

---
+++ applications/frontend/src/widgets/profile/ui.tsx
@@ -0,0 +1,12 @@
+import { DialogDescription, DialogHeader, DialogTitle } from '@/shared/ui'
+
+export const Profile = () => {
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>It's profile</DialogTitle>
+        <DialogDescription>Example</DialogDescription>
+      </DialogHeader>
+    </>
+  )
+}
