34a4e6d5320df385a0f66ec7171bebf6226f29cf
Erik <slmv_e@icloud.com>
2024-11-03 06:36:50 +0300

    ✨ feat: added svgr

--- applications/frontend/vite.config.ts
+++ applications/frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'node:path'
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
